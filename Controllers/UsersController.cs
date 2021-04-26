476a52b3c7369b793ee57b5be14b40f8be85f6a5
dasignis <jana.zadvernyak@gmail.com>
2021-04-26 23:22:53 +0300

    added creating posts only for logged in user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,8 +1,6 @@
 ﻿using Lab04.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
